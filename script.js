9e9a50bc3497526be9a1e0e0640d1dc282053101
Suansh Budhiraja <43946155+ishaansharma750@users.noreply.github.com>
2023-05-30 18:01:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -48,6 +48,51 @@ Promise.all(promises).then(function (results) {
 
 
 
+// const output = document.getElementById('output');
+// // const table = document.querySelector('table');
+
+// const row = output.insertRow(-1);
+// const col1 = row.insertCell(0);
+// const col2 = row.insertCell(1);
+// col1.innerText = "Loading...";
+// col2.innerText = "Loading...";
+
+// const promises = [
+// 	new Promise (resolve => {
+// 		setTimeout(() => {
+// 			resolve();
+// 			return "Promise 1";
+// 		}, Math.random() * 3000);
+// 	}),
+
+// 	new Promise (resolve => {
+// 		setTimeout(() => {
+// 			resolve();
+// 		}, Math.random() * 3000);
+// 	}),
+
+// 	new Promise (resolve => {
+// 		setTimeout(() => {
+// 			resolve()
+// 		}, Math.random() * 3000);
+// 	})
+// ];
+
+// const timeArray = [];
+// Promise.all(promises).then((result) => {
+
+// 	row.remove();
+
+// 	const start = performance.now();
+// 	const tableRow = output.insertRow(-1);
+// 	const tableCel1 = tableRow.insertCell(0);
+// 	const tableCel2 = tableRow.insertCell(1);
+// 	const end = performance.now();
+// 	const time = end - start;
+// 	timeArray.push(time);
+// 	tableCel1.innerText = value;
+// });
+
 
 
 
